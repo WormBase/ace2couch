90d29230d1cc909da388b3ed98ea2ad5af54098f
Adrian Duong <adrian.duong@oicr.on.ca>
2011-11-25 14:24:14 -0500

    Separate databases per class

--- runloadcouch.pl
+++ runloadcouch.pl
@@ -8,9 +8,11 @@ use warnings;
 use File::Spec;
 use Parallel::ForkManager;
 
-use constant NUM_FORKS => 3;
+use constant NUM_FORKS => 3; # number of cores available
+my $EXTENSION = ".jace";
+my $DB_PREFIX = "ws228_experimental_";
 
-my $dir = shift;
+my $dir = shift or die "Need directory\n";
 unless (-e $dir and -d $dir) {
     die "$dir is not a directory\n";
 }
@@ -23,11 +25,11 @@ mkdir 'err';
 opendir(my $dirh, $dir);
 my @files = sort readdir($dirh);
 for my $base (@files) {
-    next unless $base =~ /\.jace$/;
-    my $file = File::Spec->catfile($dir, $base);
+    next unless $base =~ s/\Q$EXTENSION\E$//;
+    my $file = File::Spec->catfile($dir, $base . $EXTENSION);
 
     $pm->start and next;
-    my $cmd = qq(perl loadcouch.pl --q @ARGV "$file" > "logs/$base.log" 2> "err/$base.err");
+    my $cmd = qq(perl loadcouch.pl --db "${DB_PREFIX}\L${base}\E" --q @ARGV "$file" > "logs/$base.log" 2> "err/$base.err");
     print $cmd, "\n";
     system($cmd);
     print 'Done ', $base, "\n";
